6c855838a7442d03a044a34ae5f629d8f9ead285
Daniel Reina <dannyflatiron@gmail.com>
2019-12-13 19:01:19 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -19,6 +19,11 @@ attr_reader :artist
   end
 
   def self.new_by_filename(file)
+    song_name = file.split(" - ")[1]
+    artist = file.split(" - ")[0]
+    song = self.new(song_name)
+    song.artist_name = artist
+    song
   end
 
 end
